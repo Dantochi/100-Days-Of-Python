22ca3420fe61e074a90bc6c62a19b17428e2a2ad
Tochukwu Chidi <chiditochukwudaniel@gmail.com>
2023-09-09 23:22:02 +0100

    today is 9th September, 2023. Did some work on pomodoro project, remaining one module on it and then I'm done. I have to finish the revision, then continue the course proper. Ok bye!!!

--- Day-28-Pomodoro-Project/main.py
+++ Day-28-Pomodoro-Project/main.py
@@ -1,3 +1,4 @@
+import math
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -9,24 +10,64 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps = + 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+    if reps % 2 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="Break", fg=RED)
+    elif reps % 8 == 0:
+        count_down(short_break_sec)
+        timer_label.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="Work", fg=GREEN)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec <= 9:
+        count_sec = f"0{count_sec}"  # Applying the technique if dynamic typing
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)  # The integers indicate milliseconds
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), bg=YELLOW)
+timer_label.grid(column=2, row=1)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)  # Used to display images in the GUI
 tomato_img = PhotoImage(
     file="tomato.png")  # Used to read the images that are to be displayed on the canvas and the create_image method
 # expects the image directly from this class
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=2, row=2)
 
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=1, row=3)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=3, row=3)
+
+check_marks = Label(text="✔️", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=2, row=4)
 window.mainloop()
