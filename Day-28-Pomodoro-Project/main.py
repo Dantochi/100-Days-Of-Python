6690e1c42349122ce6544a65aba93767b2eb81cd
Tochukwu Chidi <chiditochukwudaniel@gmail.com>
2023-09-10 22:54:35 +0100

    Today is 10th September, 2023... Did small stuff

--- Day-28-Pomodoro-Project/main.py
+++ Day-28-Pomodoro-Project/main.py
@@ -11,10 +11,17 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
-
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
@@ -25,7 +32,7 @@ def start_timer():
 
     if reps % 2 == 0:
         count_down(long_break_sec)
-        timer_label.config(text="Break", fg=REDc)
+        timer_label.config(text="Break", fg=RED)
     elif reps % 8 == 0:
         count_down(short_break_sec)
         timer_label.config(text="Break", fg=PINK)
@@ -43,9 +50,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)  # The integers indicate milliseconds
+        global timer
+        timer = window.after(1000, count_down, count - 1)  # The integers indicate milliseconds
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔️"
+        check_marks.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -65,7 +78,7 @@ canvas.grid(column=2, row=2)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=1, row=3)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=3, row=3)
 
 check_marks = Label(text="✔️", fg=GREEN, bg=YELLOW)
